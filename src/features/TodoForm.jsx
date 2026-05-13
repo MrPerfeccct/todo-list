bf179a01497deb168241779d1d9942e15fefcb8b
MrPerfeccct <1ammrperfeccct@gmail.com>
2026-05-13 00:30:58 -0500

    Add todo editing functionality

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,4 +1,6 @@
 import { useRef, useState } from 'react';
+import TextInputWithLabel from '../shared/TextInputWithLabel.jsx';
+import { isValidTodoTitle } from '../utils/todoValidation';
 
 function TodoForm({ onAddTodo }) {
   const [workingTodoTitle, setWorkingTodoTitle] = useState('');
@@ -18,18 +20,15 @@ function TodoForm({ onAddTodo }) {
 
   return (
     <form onSubmit={handleAddTodo}>
-      <label htmlFor="todoTitle">Todo</label>
-      <input
+      <TextInputWithLabel
+        elementId="todoTitle"
+        labelText="Todo"
         ref={inputRef}
-        type="text"
-        id="todoTitle"
-        name="todoTitle"
-        placeholder="Todo text"
         value={workingTodoTitle}
         onChange={(event) => setWorkingTodoTitle(event.target.value)}
-        required
       />
-      <button type="submit" disabled={!workingTodoTitle.trim()}>
+
+      <button type="submit" disabled={!isValidTodoTitle(workingTodoTitle)}>
         Add Todo
       </button>
     </form>
